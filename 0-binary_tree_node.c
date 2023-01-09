93813e4d86fe9a4c55d765635b8d867d50aa05d8
Soniabensaad <5687@holbertonstudents.com>
2023-01-09 12:19:18 +0100

    new node binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node = malloc(sizeof(binary_tree_t));
+    if (node == NULL)
+    {
+        return (NULL);
+    }
+    node->n = value;
+    node->left = NULL;
+    node->right = NULL;
+    node->parent = parent;
+    return (node);
+
+}
