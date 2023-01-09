ad824ffb0e22c3ae60259bc042a6b99dacddb093
Soniabensaad <5687@holbertonstudents.com>
2023-01-09 15:58:57 +0100

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,28 +9,34 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-    {
-        return (NULL);
-    }
-    node->n = value;
-    node->parent = parent;
-    node->right = NULL;
-    node->left = NULL;
-    if (parent->left == NULL)
-    {
+    binary_tree_t *node;
+    binary_tree_t *tmp;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->right = NULL;
+	node->left = NULL;
+
+	if (parent->left == NULL)
+	{
+		parent->left = node;
+        node->parent = parent;
+	}
+	else
+	{
+		tmp = parent->left;
         parent->left = node;
-    }
-    else
-    {
-        node->left = parent->left;
-        parent->left->parent = node;
-        parent->left = node;
-    }
-    return (node);
+        node->left = tmp;
+        node->parent = parent;
+        tmp->parent = node;
+
+	}
+	return (node);
 }
