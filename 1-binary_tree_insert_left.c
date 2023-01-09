a93602500d404be238c0e4585e91f0bf25fe58d1
Soniabensaad <5687@holbertonstudents.com>
2023-01-09 15:28:38 +0100

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,8 +17,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     node->left = NULL;
     if (parent->left == NULL)
     {
-        parent->left = node->left;
-        node->parent = parent;
+        parent->left = node;
+    
     }
     
     else
